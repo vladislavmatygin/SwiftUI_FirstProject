a865b969950958b89ae184a71a17877d80900e92
vladislavmatygin <vladislavmatygin@yandex.by>
2023-04-09 21:21:50 +0200

    Alert & ActionSheet

--- SwiftUI_FirstProject/SwiftUI_FirstProject/ContentView.swift
+++ SwiftUI_FirstProject/SwiftUI_FirstProject/ContentView.swift
@@ -1,14 +1,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isError = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+//        Choose functions:
+//        showAlert()
+        showActionSheet()
+    }
+    
+    fileprivate func showAlert() -> some View {
+        Button(action: {
+            self.isError = true
+        }, label: {
+            Text("Log in")
+        }).alert(isPresented: $isError, content: {
+            Alert(title: Text("Title"),
+                  message: Text("Are you sure?"),
+                  primaryButton: .destructive(Text("Ok"),
+                                              action: {
+                                                print("Zapka market")
+                  }), secondaryButton: .cancel())
+        })
+    }
+    
+    fileprivate func showActionSheet() -> some View {
+        Button(action: {
+            self.isError = true
+        }, label: {
+            Text("Log out")
+        }).actionSheet(isPresented: $isError, content: {
+            ActionSheet(title: Text("Loading"),
+                        message: Text("Do you want to load this photo?"),
+                        buttons: [.destructive(Text("Yes"), action: {
+                            // func for loading
+                        }), .cancel()])
+        })
     }
 }
 
